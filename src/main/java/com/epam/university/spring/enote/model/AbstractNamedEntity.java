66dece470911a6548ab27775478322ee7d463a71
Konstantin Mavropulo <konstantin.n.mavropulo@gmail.com>
2017-12-09 23:00:21 +0300

    Dev: the inheritance of entities have been fixed: only human-like entities extend AbstractNamedEntity; constructors added to entities.

--- src/main/java/com/epam/university/spring/enote/model/AbstractNamedEntity.java
+++ src/main/java/com/epam/university/spring/enote/model/AbstractNamedEntity.java
@@ -3,6 +3,17 @@ package com.epam.university.spring.enote.model;
 import lombok.Getter;
 import lombok.Setter;
 
+
+/**
+ * An abstract class designed for a person to be able to identify an entity (easier to perceive
+ * than a digital id).
+ *
+ * @see <a href="https://en.wikipedia.org/wiki/Named_entity">In the expression "Named Entity" the
+ * word "Named" aims to restrict the possible set of entities to only those for which one or many
+ * rigid designators stands for the referent.A designator is rigid when it designates the same thing
+ * in every possible world.
+ */
+
 @Getter
 @Setter
 public abstract class AbstractNamedEntity extends AbstractBaseEntity {
