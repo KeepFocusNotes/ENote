6be47a9f707f3179c6af080c5bfd259bb79702a3
Konstantin Mavropulo <konstantin.n.mavropulo@gmail.com>
2017-12-09 11:20:51 +0300

    Dev: GenericDao interface (for all technology's stacks, that could be possibly installed); Jdbc package have been added; JdbcAbstractGenericDao have been implemented, JdbcUserRepositoryImpl is under construction; README.md fixed.

---
+++ src/main/java/com/epam/university/spring/enote/repository/jdbc/JdbcAbstractGenericDao.java
@@ -0,0 +1,29 @@
+package com.epam.university.spring.enote.repository.jdbc;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Repository;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Getter
+@Setter
+@Repository
+public abstract class JdbcAbstractGenericDao<T extends Serializable> {
+    public Class<T> entityClass;
+
+    public abstract T get(Integer id);
+
+    public List<T> getAll() {
+        return null;
+    }
+
+    public abstract T save(T entity);
+
+    public abstract List<T> saveAll(List<T> entity);
+
+    public abstract boolean delete(T entity);
+
+    public abstract boolean deleteAll();
+}
