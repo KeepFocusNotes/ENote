b2ea5ece441afa450cf9426f75eaf65ba8a233d6
Konstantin Mavropulo <konstantin.n.mavropulo@gmail.com>
2017-12-10 19:41:35 +0300

    Dev: UserServiceTest's methods getById(), getAll(), create(), update() implemented, the other's implementing is in progress. Debugging in progress.

--- src/test/java/com/epam/university/spring/enote/services/jdbc/UserServiceTest.java
+++ src/test/java/com/epam/university/spring/enote/services/jdbc/UserServiceTest.java
@@ -1,5 +1,6 @@
 package com.epam.university.spring.enote.services.jdbc;
 
+import com.epam.university.spring.enote.model.User;
 import com.epam.university.spring.enote.services.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,6 +9,15 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.epam.university.spring.enote.UserTestData.USER_FIRST;
+import static com.epam.university.spring.enote.UserTestData.USER_FIRST_ID;
+import static com.epam.university.spring.enote.UserTestData.assertMatch;
+import static org.junit.Assert.assertTrue;
 
 @ContextConfiguration({
         "classpath:spring/spring-app.xml",
@@ -19,20 +29,37 @@ public class UserServiceTest {
 
     @Autowired
     private UserService userService;
+
     @Test
     public void getById() throws Exception {
+        User actual = userService.getById(USER_FIRST_ID);
+        assertMatch(USER_FIRST, actual);
     }
 
     @Test
     public void getAll() throws Exception {
+        List<User> usersAll = userService.getAll();
+        assertTrue(16403 == usersAll.size());
     }
 
     @Test
     public void create() throws Exception {
+        List<User> usersAllBeforeCreation = userService.getAll();
+        User newUser = new User(null, "new@gmail.com", "newPass", LocalDate.of(
+                2000, 01, 01));
+        User created = userService.create(newUser);
+        newUser.setId(created.getId());
+        usersAllBeforeCreation.add(newUser);
+        ArrayList usersAllBeforeCreationPlusNew = new ArrayList<>(usersAllBeforeCreation);
+        assertMatch(userService.getAll(), usersAllBeforeCreationPlusNew);
     }
 
     @Test
     public void update() throws Exception {
+        User userToUpdate = new User(USER_FIRST);
+        userToUpdate.setPassword("UpdatedPassword");
+        userService.update(userToUpdate);
+        assertMatch(userService.getById(USER_FIRST_ID), userToUpdate);
     }
 
     @Test
