a247f4d039f79e395afc7d2bfc8b9f717cc38730
Konstantin Mavropulo <konstantin.n.mavropulo@gmail.com>
2017-12-11 07:42:01 +0300

    Dev: NotepadServiceTest have been implemented, initDB.sql configured, populateDB.sql configured, NotepadTestData class have been implemented.

---
+++ src/test/java/com/epam/university/spring/enote/services/jdbc/NotepadServiceTest.java
@@ -0,0 +1,112 @@
+package com.epam.university.spring.enote.services.jdbc;
+
+import com.epam.university.spring.enote.model.AbstractBaseEntity;
+import com.epam.university.spring.enote.model.Notepad;
+import com.epam.university.spring.enote.services.NotepadService;
+import com.epam.university.spring.enote.util.exception.NotFoundException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Comparator;
+import java.util.List;
+
+import static com.epam.university.spring.enote.NotepadTestData.LIST_NOTEPADS_TO_CREATE;
+import static com.epam.university.spring.enote.NotepadTestData.NOTEPADS_INITIALIZED;
+import static com.epam.university.spring.enote.NotepadTestData.NOTEPAD_FIRST;
+import static com.epam.university.spring.enote.NotepadTestData.NOTEPAD_FIRST_ID;
+import static com.epam.university.spring.enote.NotepadTestData.NOTEPAD_LAST;
+import static com.epam.university.spring.enote.NotepadTestData.NOTEPAD_LAST_ID;
+import static com.epam.university.spring.enote.NotepadTestData.NOTEPAD_TO_CREATE;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@ContextConfiguration({
+        "classpath:spring/spring-app.xml",
+        "classpath:spring/spring-db.xml"
+})
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@RunWith(SpringRunner.class)
+@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
+public class NotepadServiceTest {
+
+    @Autowired
+    private NotepadService notepadService;
+
+    @Test
+    public void getByIdFirstNotepad() throws Exception {
+        System.out.println("1");
+        assertEquals(notepadService.getById(NOTEPAD_FIRST_ID), NOTEPAD_FIRST);
+        //assertMatch(notepadService.getById(NOTEPAD_FIRST_ID), NOTEPAD_FIRST);
+    }
+
+    @Test
+    public void getByIdLastNotepad() throws Exception {
+        System.out.println("2");
+        assertEquals(notepadService.getById(NOTEPAD_LAST_ID), NOTEPAD_LAST);
+        //assertMatch(actual,NOTEPAD_FIRST);
+    }
+
+    @Test
+    public void getAll() throws Exception {
+        System.out.println("3");
+        List<Notepad> notepadsAll = notepadService.getAll();
+        System.out.println(notepadsAll.size());
+        notepadsAll.sort(Comparator.comparing(AbstractBaseEntity::getId));
+        int counter = 1;
+        for (Notepad notepad : notepadsAll) {
+            if (counter != notepad.getId() | counter > NOTEPADS_INITIALIZED) {
+                System.out.println(notepad);
+                throw new NotFoundException("Entity with needed id not found.");
+            }
+            counter++;
+        }
+        assertTrue(NOTEPADS_INITIALIZED == notepadsAll.size());
+    }
+
+    @Test
+    public void create() throws Exception {
+        System.out.println("4");
+        NOTEPAD_TO_CREATE.setId(notepadService.create(NOTEPAD_TO_CREATE).getId());
+        assertEquals(notepadService.getById(NOTEPAD_TO_CREATE.getId()), NOTEPAD_TO_CREATE);
+    }
+
+    @Test
+    public void update() throws Exception {
+        System.out.println("5");
+        Notepad notepadToUpdate = new Notepad(NOTEPAD_FIRST);
+        notepadToUpdate.setTitle("UpdatedTitle");
+        notepadService.update(notepadToUpdate);
+        assertEquals(notepadService.getById(notepadToUpdate.getId()), notepadToUpdate);
+    }
+
+    @Test
+    public void createFromList() throws Exception {
+        System.out.println("6");
+        List<Notepad> expected = notepadService.getAll();
+        notepadService.createFromList(LIST_NOTEPADS_TO_CREATE);
+        expected.addAll(LIST_NOTEPADS_TO_CREATE);
+        List<Notepad> actual = notepadService.getAll();
+        assertTrue(actual.containsAll(expected)
+                & expected.containsAll(actual));
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void delete() throws Exception {
+        System.out.println("7");
+        notepadService.delete(NOTEPAD_FIRST);
+        notepadService.getById(NOTEPAD_FIRST.getId());
+    }
+
+    @Test
+    public void deleteAll() throws Exception {
+        System.out.println("8");
+        notepadService.deleteAll();
+        assertTrue(notepadService.getAll().size() == 0);
+    }
+}
