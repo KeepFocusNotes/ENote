5d5791e510f07d34a177c705cf5ccfa1a62e7c2a
Konstantin Mavropulo <konstantin.n.mavropulo@gmail.com>
2017-12-16 16:43:02 +0300

    Dev: switching to JPA: Notepad class have been upgraded.

--- src/main/java/com/epam/university/spring/enote/model/Notepad.java
+++ src/main/java/com/epam/university/spring/enote/model/Notepad.java
@@ -6,22 +6,39 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@Table(name = "notepads")
 public class Notepad extends AbstractBaseEntity {
+
+    @Column(name = "title", nullable = false)
+    @NotBlank
+    @Size(min = 1, max = 128)
     public String title;
-    public Integer userId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    @NotNull
+    public User user;
 
     public Notepad(Notepad notepad) {
-        this(notepad.getId(), notepad.getTitle(), notepad.getUserId());
+        this(notepad.getId(), notepad.getTitle());
     }
 
-    public Notepad(Integer id, String title, Integer userId) {
+    public Notepad(Integer id, String title) {
         super(id);
         this.title = title;
-        this.userId = userId;
     }
 }
