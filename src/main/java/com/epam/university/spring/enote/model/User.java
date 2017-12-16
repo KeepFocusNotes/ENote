3f4b7da1266ee3540c0c02bae2cfd9095e52af84
Konstantin Mavropulo <konstantin.n.mavropulo@gmail.com>
2017-12-16 15:26:22 +0300

    Dev: switching to JPA: User class have been upgraded.

--- src/main/java/com/epam/university/spring/enote/model/User.java
+++ src/main/java/com/epam/university/spring/enote/model/User.java
@@ -3,15 +3,40 @@ package com.epam.university.spring.enote.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Getter
 @Setter
+@Entity
+//TODO uniqueConstraints = {@UniqueConstraint ??
+@Table(name = "users")
 public class User extends AbstractNamedEntity {
+
+    @Column(name = "email", nullable = false, unique = true)
+    @Email
+    @NotBlank
+    @Size(max = 100)
     private String email;
+
     //temporal field
+    @Column(name = "password", nullable = false)
+    @NotBlank
+    @Size(min = 5, max = 128)
     private String password;
+
+    @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @Column(name = "registration_date", columnDefinition = "timestamp default now()")
+    @NotNull
     private LocalDate registrationDate = LocalDate.now();
 
     public User() {
@@ -40,6 +65,7 @@ public class User extends AbstractNamedEntity {
                 '}';
     }
 
+    //TODO optimize
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
