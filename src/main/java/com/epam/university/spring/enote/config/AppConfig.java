79b50384180729277e51d84391565e0b333555e9
Konstantin Mavropulo <konstantin.n.mavropulo@gmail.com>
2017-12-23 21:32:48 +0300

    Dev: AppConfig upgraded - datasource configuration has been upgraded; transaction isolation level changed to Serializable.

--- src/main/java/com/epam/university/spring/enote/config/AppConfig.java
+++ src/main/java/com/epam/university/spring/enote/config/AppConfig.java
@@ -1,15 +1,17 @@
 package com.epam.university.spring.enote.config;
 
-import java.sql.SQLException;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -23,36 +25,60 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @ImportResource("classpath:aspect-conf.xml")
 public class AppConfig {
-  @Bean
-  public JpaVendorAdapter jpaVendorAdapter() {
-    HibernateJpaVendorAdapter bean = new HibernateJpaVendorAdapter();
-    bean.setDatabase(Database.H2);
-    bean.setGenerateDdl(true);
-    return bean;
-  }
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter bean = new HibernateJpaVendorAdapter();
+        bean.setDatabase(Database.H2);
+        bean.setGenerateDdl(true);
+        return bean;
+    }
 
-  @Bean
-  public LocalContainerEntityManagerFactoryBean entityManagerFactory(
-      DataSource dataSource, JpaVendorAdapter jpaVendorAdapter) {
-    LocalContainerEntityManagerFactoryBean bean = new LocalContainerEntityManagerFactoryBean();
-    bean.setDataSource(dataSource);
-    bean.setJpaVendorAdapter(jpaVendorAdapter);
-    bean.setPackagesToScan("com.epam");
-    return bean;
-  }
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(
+            DataSource dataSource, JpaVendorAdapter jpaVendorAdapter) {
+        LocalContainerEntityManagerFactoryBean bean = new LocalContainerEntityManagerFactoryBean();
+        bean.setDataSource(dataSource);
+        bean.setJpaVendorAdapter(jpaVendorAdapter);
+        bean.setPackagesToScan("com.epam");
+        return bean;
+    }
 
-  @Bean
-  public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
-    return new JpaTransactionManager(emf);
-  }
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
+        return new JpaTransactionManager(emf);
+    }
 
-  @Bean
-  public DataSource dataSource() throws SQLException {
-    return new EmbeddedDatabaseBuilder().setName("test").
-        setType(EmbeddedDatabaseType.H2)
-        .addScript("classpath:db/initDB.sql")
-        .addScript("classpath:db/populateDB.sql")
-        .build();
-  }
+   /* @Bean
+    public DataSource dataSource() throws SQLException {
+        return new EmbeddedDatabaseBuilder().setName("test")
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("classpath:db/initDB.sql")
+                .addScript("classpath:db/populateDB.sql")
+                .build();
+    }*/
 
+    @Bean(name = "dataSource")
+    public DataSource dataSource() {
+        DataSource dataSource = createDataSource();
+        DatabasePopulatorUtils.execute(databasePopulator(), dataSource);
+        return dataSource;
+    }
+
+    private DatabasePopulator databasePopulator() {
+        ResourceDatabasePopulator databasePopulator = new ResourceDatabasePopulator();
+        databasePopulator.setContinueOnError(true);
+        databasePopulator.addScript(new ClassPathResource("db/initDB.sql"));
+        databasePopulator.addScript(new ClassPathResource("db/populateDB.sql"));
+        return databasePopulator;
+    }
+
+    private SimpleDriverDataSource createDataSource() {
+        SimpleDriverDataSource simpleDriverDataSource = new SimpleDriverDataSource();
+        simpleDriverDataSource.setDriverClass(org.h2.Driver.class);
+        simpleDriverDataSource.setUrl("jdbc:h2:mem:test;LOCK_MODE=1;DB_CLOSE_DELAY=-1;" +
+                "ACCESS_MODE_DATA=rws;PAGE_SIZE=4");
+        simpleDriverDataSource.setUsername("");
+        simpleDriverDataSource.setPassword("");
+        return simpleDriverDataSource;
+    }
 }
